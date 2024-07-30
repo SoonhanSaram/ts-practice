e4920621ffbde00dffdc64e29d8653d92b68d779
kkm <kyengmin911@naver.com>
2024-07-30 17:29:30 +0900

    menu 호출, 저장

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import countReducer from "../app/features/admin/count";
+import menuReducer from "../app/features/admin/menu";
 
 export const store = configureStore({
     reducer: {
         count: countReducer,
+        menu : menuReducer,
     },
 });
