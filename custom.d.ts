c0a0b63eae9978fabed0ef38cbf15622b40ae51c
kkm <kyengmin911@naver.com>
2024-10-10 09:00:55 +0900

    banner

---
+++ custom.d.ts
@@ -0,0 +1,16 @@
+import { decl } from "postcss";
+
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: any;
+  export default content;
+}
--- src/app/admin/PID0301/Insert/page.tsx
+++ src/app/admin/PID0301/Insert/page.tsx
@@ -16,66 +16,63 @@ const Insert = () => {
   const [endDate, setEndDate] = useState(new Date());
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
-  const [startDisplayTime, setStartDisplayTime] = useState("00:00");
-  const [endDisplayTime, setEndDisplayTime] = useState("00:00");
-  const [isFocused, setIsFocused] = useState(false);
+  const [imageFile, setImageFile] = useState("");
+  const [previewURL, setPreviewURL] = useState("");
 
-  // 시간 마스킹을 위한 함수
-  const maskTime = (val: string) => {
-    const hours = val.substring(0, 2) || "00";
-    const minutes = val.substring(2, 4) || "00";
-    return `${hours}:${minutes}`;
+  const defaultImage = (e: any) => {
+    e.target.src = null;
+  };
+
+  const selectImage = (e: any) => {
+    const file = e.target.files[0];
+
+    const getURL = URL.createObjectURL(file);
+    setPreviewURL(getURL);
+
+    console.log(file);
   };
 
   //  사용자가 입력할 때의 처리
-  const handleChange = (e: any, setTime: any, setDisplayTime: any) => {
+  const handleChange = (e: any, setTime: any) => {
     let input = e.target.value.replace(/\D/g, ""); // 숫자가 아닌 모든 문자 제거
+
+    console.log(input);
+
+    if (input.length === 0) {
+      setTime("");
+      return;
+    }
 
     // 최대 4자리 숫자로 제한
     if (input.length > 4) {
       input = input.substring(0, 4);
     }
 
-    // 입력한 숫자에 따라 포맷 적용
-    let formattedInput;
-    if (input.length <= 2) {
-      // 2자리 미만일 경우 HH만 표시
-      formattedInput = input.padEnd(2, "0");
-    } else {
-      // 3자리 이상일 경우 HH:MM으로 표시
-      const hours = input.substring(0, 2);
-      const minutes = input.substring(2, 4).padEnd(2, "0");
-      formattedInput = `${hours}${minutes}`;
-    }
+    // let hour = input.length >= 2 ? parseInt(input.substring(0, 2)) : null;
+    // let minute = input.length === 4 ? parseInt(input.substring(2, 4)) : null;
 
-    // 시간 범위체크 (23:59 이내로 제한)
+    let hour = parseInt(input.substring(0, 2));
+    let minute = input.length >= 3 ? parseInt(input.substring(2, 4)) : null;
 
-    let hour = parseInt(input.substring(0, 2)) || 0;
-    let minute = parseInt(input.substring(2, 4)) || 0;
-
-    if (hour > 23) {
+    if (hour !== null && hour > 23) {
       hour = 23;
     }
 
-    if (minute > 59) {
+    if (minute !== null && minute > 59) {
       minute = 59;
     }
 
-    setTime(formattedInput);
-    setDisplayTime(formattedInput);
-  };
+    // 시간 또는 분이 null일 경우 빈 문자열 처리
+    const formattedHour = hour !== null ? hour.toString().padStart(2, "") : "";
+    const formattedMinute =
+      minute !== null ? minute.toString().padStart(2, "") : "";
 
-  const handleFocus = (time: any, setDisplayTime: any) => {
-    setIsFocused(true);
-    if (time === "") setDisplayTime("");
-  };
+    // 완전한 시간이 입력되었을 경우 HH:MM 형식으로 표시, 아니라면 시간만 표시
+    const formattedTime = formattedMinute
+      ? `${formattedHour}:${formattedMinute}`
+      : formattedHour;
 
-  // 포커스가 해제되었을 때 마스킹 처리
-  const handleBlur = (time: any, setDisplayTime: any) => {
-    setIsFocused(false);
-    if (time === "") {
-      setDisplayTime("00:00"); // 빈 값일 경우 기본 마스크값 표시
-    }
+    setTime(formattedTime);
   };
 
   return (
@@ -83,72 +80,105 @@ const Insert = () => {
       <h4>배너 등록</h4>
       <div className="contents-box">
         <div className="item">
-          <label>
-            배너 제목
-            <input type="text" />
-          </label>
-          <label>
-            배너 이미지
-            <input type="file" />
-          </label>
-          <div className="toggle-group">
-            <input
-              type="radio"
-              id="option1"
-              name="toggle"
-              checked={selected === "option1"}
-              onChange={() => setSelected("option1")}
-            />
-            <label htmlFor="option1">사용</label>
-
-            <input
-              type="radio"
-              id="option2"
-              name="toggle"
-              checked={selected === "option2"}
-              onChange={() => setSelected("option2")}
-            />
-            <label htmlFor="option2">미사용</label>
-
-            <div className={`slider ${selected}`}></div>
+          <div className="dust-class">
+            <label htmlFor="bannerTitle">배너 제목</label>
+            <input id="bannerTitle" type="text" />
           </div>
-          <label>
-            배너 위치
+          <div>
+            <h4>이미지 미리보기</h4>
+            <div
+              style={{
+                width: "150px",
+                height: "180px",
+                border: "1px solid black",
+              }}
+            >
+              <img
+                src={previewURL}
+                alt="preview img"
+                style={{ width: "100%", height: "100%", objectFit: "contain" }}
+                onError={(e) => defaultImage(e)}
+              />
+            </div>
+          </div>
+          <div className="dust-class">
+            <span>배너 사용여부</span>
+            <div className="toggle-group">
+              <input
+                type="radio"
+                id="option1"
+                name="toggle"
+                checked={selected === "option1"}
+                onChange={() => setSelected("option1")}
+              />
+              <label htmlFor="option1">사용</label>
+
+              <input
+                type="radio"
+                id="option2"
+                name="toggle"
+                checked={selected === "option2"}
+                onChange={() => setSelected("option2")}
+              />
+              <label htmlFor="option2">미사용</label>
+
+              <div className={`slider ${selected}`}></div>
+            </div>
+          </div>
+          <div className="dust-class">
+            <label>배너 위치 </label>
             <input type="number" />
-          </label>
-          <label>
-            시작일
+          </div>
+          <div className="dust-class">
+            <label>시작일</label>
             <DatePicker
               selected={startDate}
               onChange={(date) => setStartDate(date)}
             />
-          </label>
-          <label>
-            시작 시간
+          </div>
+          <div className="dust-class">
+            <label>시작 시간</label>
             <input
               type="text"
-              value={isFocused ? startDisplayTime : startTime}
-              onChange={(e) =>
-                handleChange(e, setStartTime, setStartDisplayTime)
-              }
-              onFocus={() => handleFocus(startTime, setStartDisplayTime)}
-              onBlur={() => handleBlur(startTime, setStartDisplayTime)}
+              value={startTime}
+              onChange={(e) => handleChange(e, setStartTime)}
               maxLength={5}
               placeholder="00:00"
             />
-          </label>
-          <label>
-            종료일
+          </div>
+          <div className="dust-class">
+            <label>종료일</label>
             <DatePicker
               selected={endDate}
               onChange={(date) => setEndDate(date)}
             />
-          </label>
+          </div>
           <label>
             종료 시간
-            <input type="number" />
+            <input
+              type="text"
+              value={endTime}
+              onChange={(e) => handleChange(e, setEndTime)}
+              maxLength={5}
+              placeholder="00:00"
+            />
           </label>
         </div>
+      </div>
+      <div className="button-wrapper">
+        <label>
+          <span className="button">배너 이미지</span>
+          <input
+            className="input-file"
+            type="file"
+            accept="image/*"
+            onChange={(e) => selectImage(e)}
+          />
+        </label>
+        <span className="button" onClick={() => setPreviewURL("")}>
+          배너 이미지 삭제
+        </span>
+        <span className="button">배너 저장</span>
       </div>
     </div>
   );
